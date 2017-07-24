ec386e3b98c95c710c24ee1c28aa4c39fe768782
Omkar Wagle <30377229+omkarwagle@users.noreply.github.com>
2017-07-24 18:24:54 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,35 @@
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include<string.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#define FLAGS O_WRONLY | O_CREAT | O_EXCL | O_TRUNC
+#define PERMISSION S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH
+
 int main()
 {
-    int fd;
-    char *c="Hello World\n";
-    fd=open("hello.txt",O_CREAT|O_WRONLY|O_TRUNC,S_IRUSR|S_IWUSR|S_IXUSR|S_IRGRP|S_IWGRP|S_IROTH);
-    if(fd==-1)
-        perror("ERROR!!!\n");
-    ssize_t nr=write(fd,c,strlen(c));
-    if(nr==-1)
-        perror("ERROR!!!\n");
+    /************Declaration of local variables************/
+    int fd , blockcount, blocksize , wr ;
+
+    /** Opening of file with Low level IO**/
+    fd = open ("bufio", FLAGS , PERMISSION);
+    if (fd == -1) {
+        perror ("ERROR!!!!\n") ;
+    }
+
+    /**Taking input from the user**/
+    printf ( "Enter the number of times of buffer to get printed : \n");
+    scanf ("%d", &blockcount );
+    printf("Enter blocksize : ");
+    scanf("%d", &blocksize );
+
+    char buf[blocksize];
+
+    while ( blocksize ) {
+     wr = write (fd,buf,blocksize) ;
+     blocksize--;
+    }
+
     return 0;
 }
