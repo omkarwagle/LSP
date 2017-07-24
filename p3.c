accf2578ff6fd4de5f1e65d2594bbd067fcfd2c9
Omkar Wagle <30377229+omkarwagle@users.noreply.github.com>
2017-07-24 16:50:33 +0530

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+    char buf[100];
+    int fd;
+
+    FILE * WR = fopen ("bar.txt","w");
+    if(! WR) {
+        perror ("ERROR!!!\n");
+    }
+
+    FILE * RD = fopen ("foo.txt","r");
+    if(! RD) {
+        perror("ERROR!!!!\n");
+    }
+
+    while ( fd = fread (buf , 1 , 1 , RD) > 0 ) {
+        fd = fwrite (buf , fd , 1 , WR);
+    }
+
+    fd = fseek ( WR , 0 , SEEK_END );
+    fputc('\0' , WR );
+
+    return 0;
+}
